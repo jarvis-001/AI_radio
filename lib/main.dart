de141c2e477e47e6593f38b83513f5f38c69ad52
jarvis-001 <61859855+jarvis-001@users.noreply.github.com>
2021-03-23 01:14:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alan Radio',
+      title: 'AI Radio',
       theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
